3d913f2f4683cf7a1310e3ab39725a8cb2ea9a41
Domt301 <leonardod.trimarchi@gmail.com>
2018-01-16 16:48:52 -0500

    apparently chapter 6 eliminates the filter pipe?

--- app/components/product-detail/product-detail.ts
+++ app/components/product-detail/product-detail.ts
@@ -1,14 +1,22 @@
 import {Component} from '@angular/core';
-import { ActivatedRoute} from '@angular/router';
+import {ActivatedRoute} from '@angular/router';
 import {Product, Review, ProductService} from '../../services/product-service';
+import StarsComponent from '../stars/stars';
 
 @Component({
   selector: 'auction-product-page',
-  templateUrl: 'app/components/product-detail/product-detail.html'
+  styles: ['auction-stars.large {font-size: 24px;}'],
+  templateUrl: 'app/components/product-detail/product-detail.html',
+  directives: [ StarsComponent]
 })
 export default class ProductDetailComponent {
   product: Product;
   reviews: Review[];
+
+  newComment: string;
+  newRating: number;
+
+  isReviewHidden: boolean = true;
 
   constructor(route: ActivatedRoute, productService: ProductService) {
 
@@ -16,5 +24,26 @@ export default class ProductDetailComponent {
     this.product = productService.getProductById(prodId);
 
     this.reviews = productService.getReviewsForProduct(this.product.id);
+  }
+
+  addReview() {
+    let review = new Review(0, this.product.id, new Date(), 'Anonymous',
+        this.newRating, this.newComment);
+    console.log("Adding review " + JSON.stringify(review));
+    this.reviews = [...this.reviews, review];
+    this.product.rating = this.averageRating(this.reviews);
+
+    this.resetForm();
+  }
+
+  averageRating(reviews: Review[]) {
+    let sum = reviews.reduce((average, review) => average + review.rating, 0);
+    return sum / reviews.length;
+  }
+
+  resetForm() {
+    this.newRating = 0;
+    this.newComment = null;
+    this.isReviewHidden = true;
   }
 }
